20e21872e803e769940f700edd53dfdae01feba1
Mateusz Wozniak <kontakt@mateuszwozniak.pl>
2015-10-07 22:19:17 +0200

    prepare release of v0.2.11

--- iOS/AppDelegate.m
+++ iOS/AppDelegate.m
@@ -34,7 +34,7 @@
 
   
   // point to localhost here
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
 
 
   /**
@@ -47,7 +47,7 @@
    * see http://facebook.github.io/react-native/docs/runningondevice.html
    */
 
-//  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
